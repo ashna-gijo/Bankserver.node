ea13fac450b4ff6dcbf308d7072b8c5482b8be3d
ashna <ashnagijo@gmail.com>
2021-06-01 10:39:27 +0530

    db connection

--- index.js
+++ index.js
@@ -1,35 +1,94 @@
 const express=require('express');
+const session=require('express-session');
 const dataService=require('./services/data.service');
 const app= express();
 
+app.use(session({
+    secret:'randomsecurestring',
+    resave:false,
+    saveUninitialized:false
+}));
+
 app.use(express.json());
 
+//Middleware
+app.use((req,res,next)=>{
+    console.log("Middleware");
+    next();
+})
+const logMiddleware=(req,res,next)=>{
+    console.log(req.body);
+    next();
+}
+//app.use(logMiddleware);
+
+const authMiddleware=(req,res,next)=>{
+    if(!req.session.currentUser){
+        return res.json({
+            statusCode:401,
+            status:false,
+            message:"Please Log In"
+        })
+    }
+    else{
+        next();
+    }
+}
+
+//GET Method-READ
 app.get('/',(req,res)=>{
     res.status(401).send("THIS IS A GET METHOD");
 });
+
+//POST Method-CREATE
 
 app.post('/',(req,res)=>{
     res.send("THIS IS A POST METHOD");
 });
 
+//Register
 app.post('/register',(req,res)=>{
-    const result=dataService.register(req.body.uname,req.body.acno,req.body.pswd); 
-       console.log(res.status(result.statusCode).json(result));
+    const result=dataService.register(req.body.uname,req.body.acno,req.body.pswd)
+    .then(result=>{
+    res.status(result.statusCode).json(result)
+   //res.status(200).send("success")
+    }) 
+    
+    
 });
 
-
+//login
 app.post('/login',(req,res)=>{
-    const result=dataService.login(req.body.acno,req.body.pswd); 
+    dataService.login(req,req.body.acno,req.body.pswd) 
+    .then(result=>{
+    res.status(result.statusCode).json(result)
+    })
+});
+
+
+//Deposit
+app.post('/deposit',authMiddleware,(req,res)=>{
+    const result=dataService.deposit(req.body.acno,req.body.pswd,req.body.amount); 
+       console.log(res.status(result.statusCode).json(result));
+});
+//Withdraw
+app.post('/withdraw',authMiddleware,(req,res)=>{
+    const result=dataService.withdraw(req.body.acno,req.body.pswd,req.body.amount); 
        console.log(res.status(result.statusCode).json(result));
 });
 
 
+//PUT Method
 app.put('/',(req,res)=>{
     res.send("THIS IS A PUT METHOD");
 });
+
+//PATCH Method
 app.patch('/',(req,res)=>{
     res.send("THIS IS A PATCH METHOD");
 });
+
+//DELETE Method
 app.delete('/',(req,res)=>{
     res.send("THIS IS A DELETE METHOD");
 });
--- services/data.service.js
+++ services/data.service.js
@@ -1,5 +1,7 @@
 
-let currentUser
+
+const db=require('./db');
+//let currentUser;
 let accountDetails={
     1000:{acno:1000,username:"userone",password:"userone",balance:50000},
     1001:{acno:1001,username:"usertwo",password:"usertwo",balance:5000},
@@ -9,67 +11,147 @@ let accountDetails={
   
  const register =(uname,acno,pswd)=>{
     
-    let user=accountDetails;
-    if(acno in user){
-        
-      return {
-          statusCode:422,
-          status:false,
-          message:"User Exit please Login"
-      }
-    
-  } 
+
+ return db.User.findOne({acno})
+.then(user=>{
+  console.log(user);
+  if(user){
+    return {
+      statusCode:422,
+      status:false,
+      message:"User Exit please Login"
+  }
+
+  }
   else{
-    user[acno]={
+    const newUser=new db.User({
       acno,
       username:uname,
       password:pswd,
       balance:0
-    }
-    
- return{
-     statusCode:200,
-    status:true,
-    message:"Succesfully registerd"
- } 
+    })
+    newUser.save();
+    return{
+      statusCode:200,
+     status:true,
+     message:"Successfully registerd"
+  } 
+
+  }
+ 
+})
   
-  }
 
   }
 
- const login=(acno,pswd)=>{
-    let user=accountDetails;
-    
-    if(acno in user){
-        if(pswd==user[acno]["password"]){
-          currentUser=user[acno]["password"]
-          
-          return {
-              statusCode:200,
-              status:true,
-              message:"Successfuly Login"
-            
-        }}
-        else{
-            return{
-                statusCode:422,
-                status:false,
-                message:"Incorrect Password"
-            }
-            
+ const login=(req,accno,password)=>{
+   var acno=parseInt(accno);
+   console.log(acno)
+   return db.User.findOne({acno,password})
+   .then(user=>{
+     console.log(user)
+     if(user){
+      req.session.currentUser=user;
+      return {
+        statusCode:200,
+        status:true,
+        message:"Successfully Login"
+      }
+     }
+     else{
+      return{
+        statusCode:422,
+        status:false,
+        message:"Invalid credentials"
+    }
+     }
+   })
+  }
+
+  const deposit=(req,acno,pswd,amt)=>{
+    if(!req.session.currentUser){ 
+         return {
+            statusCode:401,
+            status:false,
+           message:"Please Log In"
         }
     }
-    else{
+    var amount=parseInt(amt);
+    let user=accountDetails;
+    if(acno in user){
+        if(pswd==user[acno]["password"]){
+            user[acno]["balance"]+=amount;
+        return{
+            statusCode:200,
+            status:true,
+            balance:user[acno]["balance"],
+            message:amount+"credited and new balance is:"+user[acno]["balance"]
+    
+        }
+      }
+      
+      else{
+        return{
+            statusCode:422,
+            status:false,
+            message:"Incorrect Password"
+        }
+      }
+    }
+      else{
         return{
             statusCode:422,
             status:false,
             message:"Invalid Account"
         }
-        
-    }
+      }
+  }
 
+  const withdraw=(acno,pswd,amt)=>{
+    var amount=parseInt(amt);
+    let user=accountDetails;
+    if(acno in user){
+      if(pswd==user[acno]["password"]){
+        if(user[acno]["balance"]>amount){
+          user[acno]["balance"]-=amount;
+          
+          return {
+            statusCode:200,
+            status:true,
+            balance:user[acno]["balance"],
+            message:amount+"debited and new balance is: "+user[acno]["balance"]
+        }
+    }
+        else{
+          return{
+          statusCode:422,
+          status:false,
+          message:"Insufficient Balance"
+        }
+      }
+    }
+      else{
+       
+        return{
+            statusCode:422,
+            status:false,
+            message:"Incorrect Password"
+        }
+        
+
+      }
+      }
+      else{
+        return{
+         statusCode:200,
+         status:false,
+         message:"Invalid Account"
+        } 
+      }
   }
   module.exports={
       register,
-      login
+      login,
+      deposit,
+      withdraw
   }
